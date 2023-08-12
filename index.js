386707e5ebc48bf0f0623b7727c76cdcaedd8743
Kenneth Westbrook <kennethwestbrook17@gmail.com>
2023-08-12 14:51:52 -0700

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project title:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter your project description:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter your project installation:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter your project usage:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select your license',
+        choices: ['MIT License',
+            'Apache License 2.0',
+            'GNU General Public License v3.0',
+            'BSD 3-Clause License',
+            'Mozilla Public License 2.0',
+            'GNU Affero General Public License v3.0',
+            'The Unlicense',
+            'No License']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter your contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter your test guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your github username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
